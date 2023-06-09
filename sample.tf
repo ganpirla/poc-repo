1591c2730ab1273dfdbbe89370a915c0f22e772f
Pirla Gangadhara Rao <60540333+ganpirla@users.noreply.github.com>
2023-06-09 15:25:53 +0530

    Update sample.tf

--- sample.tf
+++ sample.tf
@@ -1,4 +1,3 @@
-
 resource "oci_network_load_balancer" "internal_lb" {
   compartment_id   = var.compartment_ocid
   display_name     = "InternalLB"
@@ -8,12 +7,31 @@ resource "oci_network_load_balancer" "internal_lb" {
   subnet_id        = oci_core_subnet.subnet[0].id
 
   backend {
-    subnet_id = oci_core_subnet.subnet[0].id
-    ip_address = oci_core_instance.compute_instance[0].private_ip
+    subnet_id   = oci_core_subnet.subnet[0].id
+    ip_address  = oci_core_instance.compute_instance[0].private_ip
   }
 
   backend {
-    subnet_id = oci_core_subnet.subnet[1].id
-    ip_address = oci_core_instance.compute_instance[1].private_ip
+    subnet_id   = oci_core_subnet.subnet[1].id
+    ip_address  = oci_core_instance.compute_instance[1].private_ip
+  }
+
+  listener {
+    name         = "HTTPListener"
+    port         = 80
+    backend_port = 8080
+    protocol     = "HTTP"
+  }
+
+  listener {
+    name         = "HTTPSListener"
+    port         = 443
+    backend_port = 8443
+    protocol     = "HTTPS"
+    ssl_configuration {
+      certificate_name = "SSL Certificate"
+      verify_depth     = 1
+      verify_peer_certificate = true
+    }
   }
 }
