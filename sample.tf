181d20bb9862dc306df316806d0503a355f8877d
Pirla Gangadhara Rao <60540333+ganpirla@users.noreply.github.com>
2023-06-09 15:21:03 +0530

    Update sample.tf

--- sample.tf
+++ sample.tf
@@ -1,1 +1,19 @@
 
+resource "oci_network_load_balancer" "internal_lb" {
+  compartment_id   = var.compartment_ocid
+  display_name     = "InternalLB"
+  shape_name       = "100Mbps"
+  is_private       = true
+
+  subnet_id        = oci_core_subnet.subnet[0].id
+
+  backend {
+    subnet_id = oci_core_subnet.subnet[0].id
+    ip_address = oci_core_instance.compute_instance[0].private_ip
+  }
+
+  backend {
+    subnet_id = oci_core_subnet.subnet[1].id
+    ip_address = oci_core_instance.compute_instance[1].private_ip
+  }
+}
